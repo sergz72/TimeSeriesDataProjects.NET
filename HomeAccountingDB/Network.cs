611c002bb0e2a247bc681e6209a79091b1e94ac5
Serhii Zashchelkin <serg.zz@ukr.net>
2024-02-21 18:35:15 +0100

    Updates

--- HomeAccountingDB/Network.cs
+++ HomeAccountingDB/Network.cs
@@ -13,17 +13,32 @@ internal enum DbCommand
     DeleteOp
 }
 
+internal readonly struct NetworkCommand(DbCommand command, int date1 = 0, int operationId = 0, FinanceOperation? operation = null) : INetworkCommand
+{
+    public byte[]? ExecuteCommand(ICommandDecoder decoder)
+    {
+        var db = ((CommandDecoder)decoder).Database!;
+        var d1 = date1;
+        var opId = operationId;
+        return command switch
+        {
+            DbCommand.Dicts => PacketHandler.BuildResponse(() => db.GetDicts()),
+            DbCommand.GetOps => PacketHandler.BuildResponse(() => db.GetOps(d1)),
+            DbCommand.ModifyOp => PacketHandler.BuildResponse(() => db.GetDicts()),
+            DbCommand.AddOp => PacketHandler.BuildResponse(() => db.GetDicts()),
+            DbCommand.DeleteOp => PacketHandler.BuildResponse(() => db.DeleteOperation(d1, opId)),
+            _ => throw new NetworkException("unknown database command")
+        };
+    }
+}
+
 internal class CommandDecoder(string dataFolderPath, int maxTimeEntries): ICommandDecoder
 {
-    private Db? _database;
-    private DbCommand _command = DbCommand.Dicts;
-    private int _operationId;
-    private int _date1;
-    private FinanceOperation? _operation;
+    internal Db? Database;
     
-    public ICommandDecoder Build(byte[] command)
+    public INetworkCommand Build(BinaryReader command)
     {
-        var cmd = command[0];
+        var cmd = command.ReadByte();
         if (!Enum.IsDefined(typeof(DbCommand), cmd))
             throw new NetworkException("wrong database command");
         int date;
@@ -32,63 +47,34 @@ internal class CommandDecoder(string dataFolderPath, int maxTimeEntries): IComma
         switch ((DbCommand)cmd)
         {
             case DbCommand.Dicts:
-                CheckLength(command.Length, 1, 1);
-                return new CommandDecoder(dataFolderPath, maxTimeEntries)
-                    { _database = this._database, _command = DbCommand.Dicts };
+                return new NetworkCommand(DbCommand.Dicts);
             case DbCommand.GetOps:
-                CheckLength(command.Length, 5, 5); // 1 - command id + 4 - date length
-                date = BitConverter.ToInt32(command, 1);
-                return new CommandDecoder(dataFolderPath, maxTimeEntries)
-                    { _database = this._database, _command = DbCommand.GetOps, _date1 = date };
+                date = command.ReadInt32();
+                return new NetworkCommand(DbCommand.GetOps, date);
             case DbCommand.ModifyOp:
-                CheckLength(command.Length, 10, int.MaxValue);
-                date = BitConverter.ToInt32(command, 1);
-                opId = BitConverter.ToInt32(command, 5);
-                op = FinanceOperation.Create(command, 9);
-                return new CommandDecoder(dataFolderPath, maxTimeEntries)
-                    { _database = this._database, _command = DbCommand.ModifyOp, _date1 = date, _operationId = opId, _operation = op };
+                date = command.ReadInt32();
+                opId = command.ReadInt32();
+                op = FinanceOperation.Create(command);
+                return new NetworkCommand(DbCommand.ModifyOp, date, opId, op);
             case DbCommand.AddOp:
-                CheckLength(command.Length, 6, int.MaxValue);
-                date = BitConverter.ToInt32(command, 1);
-                op = FinanceOperation.Create(command, 5);
-                return new CommandDecoder(dataFolderPath, maxTimeEntries)
-                    { _database = this._database, _command = DbCommand.AddOp, _date1 = date, _operation = op };
+                date = command.ReadInt32();
+                op = FinanceOperation.Create(command);
+                return new NetworkCommand(DbCommand.AddOp, date, 0, op);
             case DbCommand.DeleteOp:
-                CheckLength(command.Length, 9, 9); // 1 - command id + 4 - date length + 4 - operation id length
-                date = BitConverter.ToInt32(command, 1);
-                opId = BitConverter.ToInt32(command, 5);
-                return new CommandDecoder(dataFolderPath, maxTimeEntries)
-                    { _database = this._database, _command = DbCommand.DeleteOp, _date1 = date, _operationId = opId};
+                date = command.ReadInt32();
+                opId = command.ReadInt32();
+                return new NetworkCommand(DbCommand.AddOp, date, opId);
             default:
                 throw new NetworkException("unknown database command");
         }
-    }
-
-    private static void CheckLength(int l, int from, int to)
-    {
-        if (l < from || l > to)
-            throw new NetworkException("wrong database command length");
-    }
-
-    public byte[]? ExecuteCommand()
-    {
-        return _command switch
-        {
-            DbCommand.Dicts => PacketHandler.BuildResponse(() => _database!.GetDicts()),
-            DbCommand.GetOps => PacketHandler.BuildResponse(() => _database!.GetOps(_date1)),
-            DbCommand.ModifyOp => PacketHandler.BuildResponse(() => _database!.GetDicts()),
-            DbCommand.AddOp => PacketHandler.BuildResponse(() => _database!.GetDicts()),
-            DbCommand.DeleteOp => PacketHandler.BuildResponse(() => _database!.DeleteOperation(_date1, _operationId)),
-            _ => throw new NetworkException("unknown database command")
-        };
     }
 
     public void DatabaseInit(byte[] aesKey)
     {
         var stopwatch = new Stopwatch();
         stopwatch.Start();
-        _database = new Db(dataFolderPath, new BinaryDbConfiguration(aesKey), maxTimeEntries);
-        _database.Init();
+        Database = new Db(dataFolderPath, new BinaryDbConfiguration(aesKey), maxTimeEntries);
+        Database.Init();
         stopwatch.Stop();
         Console.WriteLine("Database initialized in {0} ms", stopwatch.ElapsedMilliseconds);
     }
--- NetworkAndCrypto/Network.cs
+++ NetworkAndCrypto/Network.cs
@@ -5,35 +5,36 @@ using TimeSeriesData;
 
 namespace NetworkAndCrypto;
 
-public sealed class NetworkException : Exception
-{
-    public NetworkException(string message) : base(message)
-    {
-    }
-}
+public sealed class NetworkException(string message) : Exception(message);
 
 public interface IPacketHandler
 {
-    byte[]? HandlePacket(byte[] data);
+    byte[]? HandlePacket(MemoryStream data);
+}
+
+public interface INetworkCommand
+{
+    byte[]? ExecuteCommand(ICommandDecoder decoder);
 }
 
 public interface ICommandDecoder
 {
-    ICommandDecoder Build(byte[] command);
-
-    byte[]? ExecuteCommand();
+    INetworkCommand Build(BinaryReader command);
 
     void DatabaseInit(byte[] aesKey)
     {
     }
 }
 
-public class PacketHandler(ICommandDecoder decoder) : IPacketHandler
+public sealed class PacketHandler(ICommandDecoder decoder) : IPacketHandler
 {
-    public byte[]? HandlePacket(byte[] data)
+    public byte[]? HandlePacket(MemoryStream data)
     {
-        var command = decoder.Build(data);
-        return command.ExecuteCommand();
+        var reader = new BinaryReader(data);
+        var command = decoder.Build(reader);
+        if (data.Length != data.Position)
+            throw new NetworkException("wrong command size");
+        return command.ExecuteCommand(decoder);
     }
 
     public static byte[] BuildResponse<T>(Func<T> func) where T: IBinaryData<T>
@@ -57,24 +58,25 @@ public class PacketHandler(ICommandDecoder decoder) : IPacketHandler
     }
 }
 
-public class AesKeyPacketHandler(ICommandDecoder decoder) : IPacketHandler
+public sealed class AesKeyPacketHandler(ICommandDecoder decoder) : IPacketHandler
 {
-    protected byte[]? AesKey;
+    private byte[]? _aesKey;
 
-    public byte[]? HandlePacket(byte[] data)
+    public byte[]? HandlePacket(MemoryStream data)
     {
-        if (data.Length < AesProcessor.KeyLength + 1)
-            throw new NetworkException("wrong packet size");
-        var command = decoder.Build(data[AesProcessor.KeyLength..]);
-        var key = data[..AesProcessor.KeyLength];
-        if (AesKey == null)
+        var reader = new BinaryReader(data);
+        var key = reader.ReadBytes(AesProcessor.KeyLength);
+        var command = decoder.Build(reader);
+        if (data.Length != data.Position)
+            throw new NetworkException("wrong command size");
+        if (_aesKey == null)
         {
-            AesKey = key;
+            _aesKey = key;
             decoder.DatabaseInit(key);
         }
-        else if (AesKey != key)
+        else if (_aesKey != key)
             throw new NetworkException("wrong AES key");
-        return command.ExecuteCommand();
+        return command.ExecuteCommand(decoder);
     }
 }
 
@@ -122,7 +124,9 @@ public sealed class TcpServer(int port, string rsaKeyFile, IPacketHandler handle
             var keyHash = SHA256.HashData(decoded[..^32]);
             if (keyHash != decoded[^32..])
                 throw new NetworkException("wrong data hash");
-            var output = handler.HandlePacket(decoded[(AesProcessor.KeyLength + AesProcessor.IvLength)..^32]);
+            var mStream = new MemoryStream(decoded, AesProcessor.KeyLength + AesProcessor.IvLength,
+                decoded.Length - 32 - AesProcessor.KeyLength + AesProcessor.IvLength);
+            var output = handler.HandlePacket(mStream);
             if (output == null) return;
             var key = decoded[..AesProcessor.KeyLength];
             var iv = decoded[AesProcessor.KeyLength..(AesProcessor.KeyLength + AesProcessor.IvLength)];
